71f72b6654381ae25be54dbd63e3ea502b08d050
Evgenii Vasilenko <gmrnsk@gmail.com>
2023-12-06 01:18:02 -0300

    migrate to pipenv

--- frontend/utils.py
+++ frontend/utils.py
@@ -5,16 +5,10 @@ import requests
 from django.core.cache import cache
 from django.utils import timezone
 
-
-log = logging.getLogger(__name__)  # noqa
-
-
+# Constants
 SCRYFALL_SETS_API_URL = "https://api.scryfall.com/sets"
-
-# Cache the call to Scryfall to avoid hitting them that much
-# Set data changes infrequently
 SCRYFALL_SETS_CACHE_KEY = "scryfall-sets"
-SCRYFALL_SETS_CACHE_DURATION = 60 * 60 * 8
+SCRYFALL_SETS_CACHE_DURATION_HOURS = 8
 
 EXCLUDED_SETS = (
     "cmb1",  # Mystery Booster Playtest Cards
@@ -34,54 +28,55 @@ EXCLUDED_SETS = (
     "scd",  # Starter Commander Decks - set has an icon issue
 )
 
+log = logging.getLogger(__name__)  # noqa
+
 
 def get_scryfall_set_data():
     cached_result = cache.get(SCRYFALL_SETS_CACHE_KEY)
     if cached_result:
+        log.debug("Using cached sets data.")
         return cached_result
 
     log.debug("Fetching sets from Scryfall API...")
-    resp = requests.get(SCRYFALL_SETS_API_URL)
-    resp.raise_for_status()
 
-    sets = resp.json()["data"]
+    def fetch_sets():
+        try:
+            resp = requests.get(SCRYFALL_SETS_API_URL)
+            resp.raise_for_status()
+            sets = resp.json().get("data", [])
+            # Save the sets back to the cache
+            cache.set(
+                SCRYFALL_SETS_CACHE_KEY, sets, SCRYFALL_SETS_CACHE_DURATION_HOURS * 3600
+            )
+            return sets
+        except requests.exceptions.RequestException as e:
+            log.error("Failed to fetch sets data from Scryfall API: %s", str(e))
+            return []
 
-    # Save the sets back to the cache
-    cache.set(SCRYFALL_SETS_CACHE_KEY, sets, SCRYFALL_SETS_CACHE_DURATION)
-
-    return sets
+    return fetch_sets()
 
 
 def get_grouped_sets():
-    sets = {}
-    for exp in get_scryfall_set_data():
-        if exp["digital"]:
-            # Skip digital only sets
-            continue
+    current_date = timezone.now().date()
 
-        if exp["set_type"] in ("memorabilia", "token", "vanguard", "promo"):
-            # Skip tokens and others sets that don't make sense
-            continue
+    def is_expansion_valid(expansion):
+        if (
+            expansion["digital"]
+            or expansion["set_type"] in ("memorabilia", "token", "vanguard", "promo")
+            or expansion["code"] in EXCLUDED_SETS
+        ):
+            return False
 
-        if exp["code"] in EXCLUDED_SETS:
-            continue
+        release_date = datetime.datetime.strptime(
+            expansion["released_at"], "%Y-%m-%d"
+        ).date()
+        return release_date <= current_date
 
-        # Skip sets released after today
-        # This is important because sets with a failing SVG URL
-        # will cause the whole process to break
-        release_date = datetime.datetime.strptime(exp["released_at"], "%Y-%m-%d").date()
-        if release_date > timezone.now().date():
-            continue
+    sets = get_scryfall_set_data()
+    grouped_sets = {}
 
+    for exp in (expansion for expansion in sets if is_expansion_valid(expansion)):
         set_type = exp["set_type"].replace("_", " ")
-        if set_type not in sets:
-            sets[set_type] = []
-        sets[set_type].append((exp["code"], exp))
+        grouped_sets.setdefault(set_type, []).append((exp["code"], exp))
 
-    grouped_sets = []
-    for set_type in sets:
-        grouped_sets.append((set_type, sets[set_type]))
-
-    grouped_sets.sort()
-
-    return grouped_sets
+    return sorted(grouped_sets.items())
--- frontend/views.py
+++ frontend/views.py
@@ -2,54 +2,80 @@ import logging
 import os
 import subprocess
 import tempfile
-import zipfile
-from pathlib import Path
 
 from django.conf import settings
-from django.http import FileResponse
+from django.http import StreamingHttpResponse
 from django.shortcuts import render
 
 from .forms import LabelGeneratorForm
 
+log = logging.getLogger(__name__)  # Create a logger
 
-log = logging.getLogger(__name__)  # noqa
+
+def generate_labels(sets, labels_template):
+    try:
+        with tempfile.TemporaryDirectory() as tempdir:
+            subprocess.check_call(
+                [
+                    "mtglabels",
+                    "--labels-per-sheet",
+                    labels_template,
+                    "--output-dir",
+                    tempdir,
+                    *sets,
+                ],
+                env={
+                    "FONTCONFIG_PATH": settings.FONTCONFIG_PATH,
+                    "PATH": os.environ["PATH"],
+                },
+                cwd=str(settings.BASE_DIR),
+            )
+            pdf_file_path = os.path.join(tempdir, "combined_labels.pdf")
+
+            if os.path.exists(pdf_file_path):
+                with open(pdf_file_path, "rb") as pdf_file:
+                    yield pdf_file.read()
+    except subprocess.CalledProcessError as e:
+        # Handle subprocess errors
+        # This may occur if the 'mtglabels' command fails
+        error_message = f"Subprocess error: {str(e)}"
+        log.error(error_message)  # Log the error message
+        # You can return an error response if needed
+    except FileNotFoundError as e:
+        # Handle file not found errors
+        # This may occur if 'mtglabels' executable is not found
+        error_message = f"File not found error: {str(e)}"
+        log.error(error_message)  # Log the error message
+        # You can return an error response if needed
+    except Exception as e:
+        # Handle other exceptions
+        # You can customize error handling here
+        error_message = f"An error occurred: {str(e)}"
+        log.error(error_message)  # Log the error message
+        # You can return an error response if needed
 
 
 def homepage(request):
     message = None
+
     if request.method == "POST":
         form = LabelGeneratorForm(request.POST)
         if form.is_valid():
             sets = form.cleaned_data["sets"]
-            # labels_template = form.cleaned_data["labels_template"]
-            labels_template = "30"
+            labels_template = form.cleaned_data.get("labels_template", "30")  # Default template value
 
-            with tempfile.TemporaryDirectory() as tempdir:
-                subprocess.check_call(
-                    [
-                        "mtglabels",
-                        "--labels-per-sheet",
-                        labels_template,
-                        "--output-dir",
-                        tempdir,
-                        *sets,
-                    ],
-                    env={
-                        "FONTCONFIG_PATH": settings.FONTCONFIG_PATH,
-                        "PATH": os.environ["PATH"],
-                    },
-                    cwd=str(settings.BASE_DIR),
+            # Generate PDF labels and stream the response
+            pdf_content = generate_labels(sets, labels_template)
+
+            if pdf_content:
+                response = StreamingHttpResponse(
+                    pdf_content,
+                    content_type="application/pdf",
                 )
-                # zipfile_path = Path(tempdir) / "labels.zip"
-                # with zipfile.ZipFile(zipfile_path, "w") as myzip:
-                #     path = Path(tempdir)
-                #     for f in path.glob("*.pdf"):
-                #         myzip.write(f, arcname=f.name)
-                # return FileResponse(open(zipfile_path, "rb"), filename="labels.zip")
-                return FileResponse(
-                    open(Path(tempdir) / "combined_labels.pdf", "rb"),
-                    filename="combined_labels.pdf",
-                )
+                response["Content-Disposition"] = 'attachment; filename="combined_labels.pdf"'
+                return response
+            else:
+                message = "PDF generation failed."
     else:
         form = LabelGeneratorForm()
 
